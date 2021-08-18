079ba9ed6fafaca5e6c14a3968c2f6dabd30c00c
ajhacker03 <58721998+ajhacker03@users.noreply.github.com>
2021-08-18 16:49:26 +0530

    Added Music Player Layout

--- app/src/main/java/com/example/radioapp/MainActivity.kt
+++ app/src/main/java/com/example/radioapp/MainActivity.kt
@@ -10,76 +10,76 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private var isPrepared = false
-    private var isStarted = false
-
-    private lateinit var button: Button
-    private lateinit var mediaPlayer: MediaPlayer
-
-    private var stream = "http://stream.radioreklama.bg:80/radio1rock128"
+//    private var isPrepared = false
+//    private var isStarted = false
+//
+//    private lateinit var button: Button
+//    private lateinit var mediaPlayer: MediaPlayer
+//
+//    private var stream = "http://stream.radioreklama.bg:80/radio1rock128"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_song)
 
-        button = findViewById(R.id.button)
-        button.isEnabled  = false
-        button.text = "Loading..."
-
-        mediaPlayer = MediaPlayer()
-        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-
-        PlayerTask().execute(stream)
-
-        button.setOnClickListener {
-            if (isStarted){
-                isStarted = false
-                mediaPlayer.pause()
-                button.text = "Play"
-            }else{
-                isStarted = true
-                mediaPlayer.start()
-                button.text = "pause"
-            }
-        }
+//        button = findViewById(R.id.button)
+//        button.isEnabled  = false
+//        button.text = "Loading..."
+//
+//        mediaPlayer = MediaPlayer()
+//        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+//
+//        PlayerTask().execute(stream)
+//
+//        button.setOnClickListener {
+//            if (isStarted){
+//                isStarted = false
+//                mediaPlayer.pause()
+//                button.text = "Play"
+//            }else{
+//                isStarted = true
+//                mediaPlayer.start()
+//                button.text = "pause"
+//            }
+//        }
 
     }
 
-    inner class PlayerTask: AsyncTask<String, Unit, Boolean>() {
-
-        override fun doInBackground(vararg params: String?): Boolean? {
-            try {
-                mediaPlayer.setDataSource(params[0])
-                mediaPlayer.prepare()
-                isPrepared = true
-            }catch (e: Exception){
-                e.printStackTrace()
-            }
-            return isPrepared
-        }
-
-        override fun onPostExecute(result: Boolean?) {
-            super.onPostExecute(result)
-            button.isEnabled = true
-            button.text = "play"
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        if(isStarted)
-            mediaPlayer.pause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        if(isStarted)
-            mediaPlayer.start()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        if(isPrepared)
-            mediaPlayer.release()
-    }
+//    inner class PlayerTask: AsyncTask<String, Unit, Boolean>() {
+//
+//        override fun doInBackground(vararg params: String?): Boolean? {
+//            try {
+//                mediaPlayer.setDataSource(params[0])
+//                mediaPlayer.prepare()
+//                isPrepared = true
+//            }catch (e: Exception){
+//                e.printStackTrace()
+//            }
+//            return isPrepared
+//        }
+//
+//        override fun onPostExecute(result: Boolean?) {
+//            super.onPostExecute(result)
+//            button.isEnabled = true
+//            button.text = "play"
+//        }
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//        if(isStarted)
+//            mediaPlayer.pause()
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        if(isStarted)
+//            mediaPlayer.start()
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        if(isPrepared)
+//            mediaPlayer.release()
+//    }
 }
